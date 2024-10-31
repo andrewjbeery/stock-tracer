69acb840b4cef4b797c81945ed923a98a95a787a
andrewjbeery <abeery@wisc.edu>
2024-10-30 22:03:18 -0500

    schedluer wroking

---
+++ daily_schedule.py
@@ -0,0 +1,18 @@
+import subprocess
+
+subprocess.check_call(["pip", "install", "-r", "requirements.txt"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+import schedule
+import time
+import os
+
+def run_script():
+    # Replace with the path to your script
+    os.system("python main.py")  # Adjust the path as needed
+
+# Schedule the job to run daily at a specific time (e.g., 8:00 AM)
+schedule.every().day.at("22:02").do(run_script)  # Adjust the time as needed
+
+while True:
+    schedule.run_pending()  # Check if a scheduled task is pending
+    time.sleep(60)  # Wait one minute before checking again
