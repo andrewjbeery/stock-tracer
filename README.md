43510151bfb33664b58a1f99bd3f195fc7bc5490
AJ Beery <andrewjbeery@gmail.com>
2024-11-13 18:07:53 -0600

    dockerfile running :)

---
+++ README.md
@@ -0,0 +1,3 @@
+Run Docker image using
+
+docker run -d -e SENDGRID_API_KEY=YOUR-API-KEY-FROM-SENDGRID stock-tracker
--- daily_schedule.py
+++ daily_schedule.py
@@ -1,17 +1,9 @@
-import subprocess
-
-subprocess.check_call(["pip", "install", "-r", "requirements.txt"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
 import schedule
 import time
-import os
-
-def run_script():
-    # Replace with the path to your script
-    os.system("python main.py")  # Adjust the path as needed
+from helpers_sendgrid import email_per_user
 
 # Schedule the job to run daily at a specific time (e.g., 8:00 AM)
-schedule.every().day.at("06:00").do(run_script)  # Adjust the time as needed
+schedule.every().day.at("13:00").do(email_per_user)  # Adjust the time as needed
 
 while True:
     schedule.run_pending()  # Check if a scheduled task is pending
